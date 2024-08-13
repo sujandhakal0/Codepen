d79e6e54d0365010a0e509df3b1d120a10f47cd5
Sujan Dhakal <sujandhakal@192.168.1.3>
2024-08-13 12:36:34 +0545

    removed collection

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -3,6 +3,4 @@ import { v4 as uuidv4 } from "uuid";
 export const Menus = [
   { id: uuidv4(), name: "Profile", uri: "/profile" },
   { id: uuidv4(), name: "Projects", uri: "/home/projects" },
-  { id: uuidv4(), name: "Collections", uri: "/home/collections" },
-  
 ];
